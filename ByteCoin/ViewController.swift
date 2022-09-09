28c8064c5e0c41c0535cd5e950b27784c770bff4
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-09 12:53:22 +0700

    Initial Commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Muhammad Ziddan Hidayatullah on 09/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
