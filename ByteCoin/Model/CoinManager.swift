13b0b67a06bd8ba7974e15bbf90bb57679050f23
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-12 10:06:02 +0700

    Connect IBOutlets, add UIPickerView delegate and data source

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,17 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Muhammad Ziddan Hidayatullah on 12/09/22.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = ""
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+}
