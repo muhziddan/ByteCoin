720fdc0035b33a3e2314440fde9100bae6e6e132
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-12 10:32:56 +0700

    make API Request

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -10,12 +10,33 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = ""
+    let apiKey = "E5AE1021-7083-4477-A972-6B711E713CC5"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     func getCoinPrice(for currency: String) {
-        
+        let url = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        request(with: url)
     }
+    
+    func request(with url: String) {
+        
+        guard let urlObject = URL(string: url) else {return}
+        let urlSession = URLSession(configuration: .default)
+        let task = urlSession.dataTask(with: urlObject) { data, response, error in
+            if error != nil {
+                print(error ?? "")
+                return
+            }
+            
+            guard let safeData = data else {return}
+            
+            guard let string = String(data: safeData, encoding: .utf8) else {return}
+            print(string)
+        }
+        
+        task.resume()
+    }
+    
     
 }
